72160758fe56874a33b2f21737e10bf7d3cec018
yenyen31 <totoro08285@naver.com>
2024-09-27 12:57:25 +0900

    advanced 응용 코드

--- week4/advanced2.php
+++ week4/advanced2.php
@@ -1,8 +1,9 @@
 <!-- 응용 for문 
- 
-
+- form: POST 사용, for사용
+- 사용자가 입력한 수의 팩토리얼을 구하는 프로그램
 -->
-<!-- <form method="POST" action="">
+
+<form method="POST" action="">
     <input type="number" name="num"  required>
     <input type="submit" value="확인">
 </form>
@@ -10,12 +11,16 @@
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") 
     {
-        $num = intval($_POST["num"]); 
-        $factorial = 1;
+        $num = intval($_POST["num"]);
+
+        $factorial = 1; // 팩토리얼 값을 저장할 변수 초기화 
+
+        // for문을 사용해 1부터 입력된 수까지 반복하며 팩토리얼 계산
         for ($i = 1; $i <= $num; $i++) 
         {
-            $factorial *= $i;
+            $factorial *= $i; // $factorial에 현재 $i 값을 곱해서 팩토리얼 계산
         }
+        // 출력
         echo "팩토리얼: " . $factorial;
     }
-?> -->
+?>

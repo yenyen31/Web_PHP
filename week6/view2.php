eabe55eda615c5fcf665350bd82002d68c3a769f
yenyen31 <totoro08285@naver.com>
2024-10-11 17:19:13 +0900

    주석 추가

--- week6/view2.php
+++ week6/view2.php
@@ -1,18 +1,23 @@
+<!-- 아이디, 비밀번호 처리 -->
+
 <html>
 
 <head>
-  <meta charset="utf-8">
-  <link href="style.css" rel="stylesheet">
+  <meta charset="utf-8"> <!-- 문서의 인코딩을 UTF-8로 설정하여 한글이 깨지지 않도록 설정 -->
+  <link href="style.css" rel="stylesheet"> <!-- 외부 CSS 파일 'style.css'를 불러와 스타일을 적용 -->
 </head>
 
 <body>
   <?php
-  // 아이디, 비밀번호 처리
-  $id = $_POST["id"];
-  $pass = $_POST["pass"];
+  // POST 방식으로 전달된 아이디와 비밀번호 값을 받아 변수에 저장
+  $id = $_POST["id"]; // 사용자가 입력한 아이디 값을 $id 변수에 저장
+  $pass = $_POST["pass"]; // 사용자가 입력한 비밀번호 값을 $pass 변수에 저장
   ?>
+
   <ul>
+    <!-- 아이디 값을 출력 -->
     <li>아 &nbsp;이 &nbsp;디 : <?= $id ?></li>
+    <!-- 비밀번호 값을 출력 -->
     <li>비밀번호 : <?= $pass ?> </li>
   </ul>
 
--- week6/view3.php
+++ week6/view3.php
@@ -1,16 +1,21 @@
+<!-- 텍스트 입력 처리 -->
+
 <html>
 
 <head>
-    <meta charset="utf-8">
-    <link href="style.css" rel="stylesheet">
+    <meta charset="utf-8"> <!-- 문서의 인코딩을 UTF-8로 설정하여 한글이 깨지지 않도록 함 -->
+    <link href="style.css" rel="stylesheet"> <!-- 외부 CSS 파일 'style.css'를 불러와 스타일을 적용 -->
 </head>
 
 <body>
     <?php
+    // POST 방식으로 전달된 'content' 값을 받아서 변수 $content에 저장
     $content = $_POST["content"];
     ?>
+
     <ul>
-        <li>글 내용 : <?= $content ?></li>
+        <!-- 사용자가 입력한 'content' 값을 출력 -->
+        <li>글 내용 : <?= htmlspecialchars($content, ENT_QUOTES, 'UTF-8') ?></li> <!-- htmlspecialchars 함수로 특수문자 처리를 통해 XSS 공격 방지 -->
     </ul>
 </body>
 

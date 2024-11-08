15b43314f093b769a0e7c5eca502bc6b0e863a89
yenyen31 <totoro08285@naver.com>
2024-11-08 13:11:51 +0900

    week9

---
+++ week9/member.sql
@@ -0,0 +1,538 @@
+CREATE TABLE MEMBER (
+   NUM      INT NOT NULL,
+   AUTO_INCREMENT,
+   ID       CHAR(20) NOT NULL,
+   NAME     CHAR(20) NOT NULL,
+   GENDER   CHAR(1),
+   POST_NUM CHAR(8),
+   ADDRESS  CHAR(80),
+   TEL      CHAR(20),
+   AGE      INT,
+   PRIMARY KEY ( NUM )
+);
+
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'yjhwang',
+           '황영주',
+           'M',
+           '100-011',
+           '서울시 중구 충무로1가',
+           '234-8879',
+           35 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'khshul',
+           '설기형',
+           'M',
+           '607-010',
+           '부산시 동래구 명륜동',
+           '764-3784',
+           33 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'chpark',
+           '박철호',
+           'M',
+           '503-200',
+           '광주시 남구 지석동',
+           '298-9730',
+           34 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'shlee',
+           '이상훈',
+           'M',
+           '503-201',
+           '광주시 남구 도금동',
+           '838-4347',
+           32 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'jyjang',
+           '장영숙',
+           'W',
+           '606-065',
+           '부산시 영도구 봉래동5가',
+           '399-9809',
+           24 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'yjbae',
+           '배용진',
+           'M',
+           '122-014',
+           '서울시 은평구 응암4동',
+           '857-5683',
+           30 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'hbpark',
+           '박혜빈',
+           'W',
+           '427-760',
+           '경기도 과천시 중앙동',
+           '234-7677',
+           22 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'mskim',
+           '김문수',
+           'M',
+           '429-020',
+           '경기도 시흥시 신천동',
+           '370-6003',
+           63 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'bkcha',
+           '차범길',
+           'M',
+           '302-121',
+           '대전시 서구 둔산1동',
+           '432-9877',
+           49 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'kskim',
+           '김길수',
+           'M',
+           '440-747',
+           '경기도 수원시 장안구 파장동',
+           '324-5875',
+           54 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'srkim',
+           '김수련',
+           'M',
+           '704-701',
+           '대구시 달서구 신당동',
+           '987-3688',
+           23 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'shlee',
+           '이성현',
+           'M',
+           '441-081',
+           '경기도 수원시 권선구 매산로1가',
+           '243-6844',
+           36 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'hnjang',
+           '정한나',
+           'W',
+           '502-763',
+           '광주시 서구 화정4동',
+           '845-4547',
+           58 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'mylee',
+           '이명연',
+           'W',
+           '502-791',
+           '광주시 서구 쌍촌동',
+           '837-9432',
+           33 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'yskim',
+           '김영숙',
+           'W',
+           '429-010',
+           '경기도 시흥시 대야동',
+           '374-8438',
+           53 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'jekim',
+           '김정은',
+           'W',
+           '503-202',
+           '광주시 남구 원산동',
+           '347-8873',
+           29 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'yjko',
+           '고영주',
+           'W',
+           '122-020',
+           '서울시 은평구 녹번동',
+           '479-3874',
+           32 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'cyahn',
+           '안철영',
+           'M',
+           '122-030',
+           '서울시 은평구 대조동',
+           '347-4687',
+           34 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'jmkim',
+           '김진모',
+           'M',
+           '530-140',
+           '전라남도 목포시 항동',
+           '379-8349',
+           28 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'ycshul',
+           '설영찬',
+           'M',
+           '606-070',
+           '부산시 영도구 청학동',
+           '983-8748',
+           41 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'jjko',
+           '고재진',
+           'M',
+           '100-013',
+           '서울시 중구 충무로3가',
+           '836-4655',
+           28 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'hwlee',
+           '이현우',
+           'M',
+           '606-071',
+           '부산시 영도구 청학1동',
+           '346-8892',
+           32 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'cskang',
+           '강찬숙',
+           'W',
+           '668-890',
+           '경상남도 남해군 설천면',
+           '377-6879',
+           21 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'ypji',
+           '지영필',
+           'M',
+           '122-040',
+           '서울시 은평구 불광동',
+           '366-3747',
+           52 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'jbkim',
+           '김진배',
+           'M',
+           '427-600',
+           '경기도 과천시 과천동',
+           '382-4993',
+           47 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'jepark',
+           '박지은',
+           'W',
+           '670-800',
+           '경상남도 거창군 거창읍',
+           '328-8743',
+           26 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'jhlee',
+           '이지현',
+           'W',
+           '704-702',
+           '대구시 달서구 월성동',
+           '386-7988',
+           27 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'bykang',
+           '강부영',
+           'M',
+           '302-120',
+           '대전시 서구 둔산동',
+           '798-3243',
+           62 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'jymoon',
+           '문진영',
+           'W',
+           '302-122',
+           '대전시 서구 둔산2동',
+           '987-3248',
+           18 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'jyjun',
+           '전지연',
+           'W',
+           '100-012',
+           '서울시 중구 충무로2가',
+           '347-2236',
+           28 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'jkko',
+           '고진길',
+           'M',
+           '122-013',
+           '서울시 은평구 응암3동',
+           '234-7466',
+           27 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'myjung',
+           '정명윤',
+           'M',
+           '502-771',
+           '광주시 서구 치평동',
+           '374-8786',
+           47 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'jsyou',
+           '유지수',
+           'W',
+           '502-772',
+           '광주시 서구 치평동',
+           '309-3897',
+           49 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'dsshin',
+           '신달성',
+           'W',
+           '530-145',
+           '전라남도 신안군 장산면',
+           '399-8789',
+           53 );
+INSERT INTO MEMBER (
+   ID,
+   NAME,
+   GENDER,
+   POST_NUM,
+   ADDRESS,
+   TEL,
+   AGE
+) VALUES ( 'sjshin',
+           '신수진',
+           'W',
+           '606-796',
+           '부산시 영도구 봉래동5가',
+           '389-8930',
+           47 );

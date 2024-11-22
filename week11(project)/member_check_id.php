146009c665831aea02098de02dacd3bfd0bc61e6
yenyen31 <totoro08285@naver.com>
2024-11-22 13:09:36 +0900

    회원가입 수정

--- week11(project)/member_check_id.php
+++ week11(project)/member_check_id.php
@@ -24,7 +24,7 @@
       if (!$id) {
          echo ("<li>아이디를 입력해 주세요!</li>");
       } else {
-         $con = mysqli_connect("localhost", "user1", "12345", "sample");
+         $con = mysqli_connect("localhost", "user1", "1111", "sample");
 
 
          $sql = "select * from members where id='$id'";
@@ -33,7 +33,7 @@
          $num_record = mysqli_num_rows($result);
 
          if ($num_record) {
-            echo "<li>" . $id . " 아이디는 중복됩니다.</li>";
+            echo "<li>" . $id . "아이디는 중복됩니다.</li>";
             echo "<li>다른 아이디를 사용해 주세요!</li>";
          } else {
             echo "<li>" . $id . " 아이디는 사용 가능합니다.</li>";
